3fa84e1ffa4020cfa522a33381e438da60dc374d
Florian Schmitt <florian.schmitt94@gmail.com>
2023-04-14 12:00:47 +0200

    edit function description

--- R/getMatchplan.R
+++ R/getMatchplan.R
@@ -1,7 +1,7 @@
 ######
 #
 # This function returns a dataframe with basic information
-# for all matches for a given set of parameters
+# for all matches for a given competitionIteration
 #
 ######
 
@@ -9,7 +9,7 @@ getMatchplan <- function(competitionIterationId, token) {
   # require packages
   require(httr)
   require(jsonlite)
-  
+
   # get competition iteration data
   response <-
     httr::GET(
@@ -20,17 +20,17 @@ getMatchplan <- function(competitionIterationId, token) {
       httr::add_headers(Authorization = base::paste("Bearer", token, sep =
                                                       " "))
     )
-  
+
   # check response status
   httr::stop_for_status(response)
-  
+
   # get data from response
   data <-
     jsonlite::fromJSON(httr::content(response, "text", encoding = "UTF-8"))$data
-  
+
   # create empty df
   matches <- base::data.frame()
-  
+
   # iterate over competitionIterationSteps
   for (i in 1:base::length(data$competitionIterationSteps$matches)) {
     # get matches for iteration
@@ -38,16 +38,16 @@ getMatchplan <- function(competitionIterationId, token) {
     # append to matches dataframe
     matches <- dplyr::bind_rows(matches, temp)
   }
-  
+
   # convert to dataframe
   matches <- jsonlite::flatten(matches)
-  
+
   # fix column names using regex
   base::names(matches) <-
     gsub("\\.(.)", "\\U\\1", base::names(matches), perl = TRUE)
   base::names(matches) <-
     gsub("Competition", "", base::names(matches), perl = TRUE)
-  
+
   # return matches
   return(matches)
 }
