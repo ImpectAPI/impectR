49e0fc7b014285d107c286b20fc531e576f2f064
Florian Schmitt <florian.schmitt94@gmail.com>
2023-04-14 12:01:31 +0200

    edit function description

--- R/getEventData.R
+++ R/getEventData.R
@@ -1,7 +1,7 @@
 ######
 #
-# This function returns a pandas dataframe that contains all events for a
-# given match
+# This function returns a dataframe that contains all events for a
+# given match ID
 #
 ######
 
@@ -10,7 +10,7 @@ getEventData <- function (match, token) {
   require(httr)
   require(jsonlite)
   require(tidyverse)
-  
+
   # get match events
   response <-
     httr::GET(
@@ -21,38 +21,38 @@ getEventData <- function (match, token) {
       ),
       httr::add_headers(Authorization = base::paste("Bearer", token, sep = " "))
     )
-  
+
   # check response status
   httr::stop_for_status(response)
-  
+
   # get data from response
   data <-
     jsonlite::fromJSON(httr::content(response, "text", encoding = "UTF-8"))$data
-  
+
   # convert to dataframe and add column with matchId
   events <- jsonlite::flatten(base::as.data.frame(data)) %>%
     dplyr::mutate(matchId = match)
-  
+
   # fix column names using regex
   base::names(events) <-
     gsub("events.", "", base::names(events), perl = TRUE)
   base::names(events) <-
     gsub("\\.(.)", "\\U\\1", base::names(events), perl = TRUE)
-  
+
   # get match data
   response <-
     httr::GET(
       base::paste0("https://api.impect.com/v4/customerapi/matches/", match),
       httr::add_headers(Authorization = base::paste("Bearer", token, sep = " "))
     )
-  
+
   # check response status
   httr::stop_for_status(response)
-  
+
   # get data from response
   data <-
     jsonlite::fromJSON(httr::content(response, "text", encoding = "UTF-8"))$data
-  
+
   # get player data and convert to dataframe and convert id column to integer
   players <- base::sapply(base::c("playerId", "commonname"),
                           function(key)
@@ -62,20 +62,20 @@ getEventData <- function (match, token) {
                             ))) %>%
     base::as.data.frame() %>%
     dplyr::mutate(playerId = base::as.integer(playerId))
-  
+
   # get match info
   match_info <- data$competition
-  
+
   # add basic info to match info
   for (key in c("matchId", "date", "dateTime")) {
     match_info[[key]] <- data[[key]]
   }
-  
+
   # Iterate over sides
   sides <- c("squadHome", "squadAway")
   info_keys <- list("name", "squadId", "squadholderType")
   country_keys <- list("id", "name")
-  
+
   for (side in sides) {
     for (key in info_keys) {
       # Add squad info
@@ -95,7 +95,7 @@ getEventData <- function (match, token) {
       match_info[side_country_name] <- side_country
     }
   }
-  
+
   # add playerName columns to events dataframe
   events <- events %>%
     dplyr::left_join(players, by = base::c("playerId" = "playerId")) %>%
@@ -108,12 +108,12 @@ getEventData <- function (match, token) {
     dplyr::rename(fouledPlayerName = commonname) %>%
     dplyr::left_join(players, by = base::c("passReceiverPlayerId" = "playerId")) %>%
     dplyr::rename(passReceiverPlayerName = commonname)
-  
+
   # add the values from the match_info list as new columns in the events dataframe
   for (name in base::names(match_info)) {
     events[[name]] <- base::rep(match_info[[name]], base::nrow(events))
   }
-  
+
   # add squadName and currentAttackingSquadName
   events <- events %>%
     dplyr::mutate(
@@ -126,21 +126,21 @@ getEventData <- function (match, token) {
         squadAwayName
       )
     )
-  
+
   # get kpi list
   response <-
     httr::GET(
       "https://api.impect.com/v4/customerapi/kpis",
       httr::add_headers(Authorization = base::paste("Bearer", token, sep = " "))
     )
-  
+
   # check response status
   httr::stop_for_status(response)
-  
+
   # get data from response
   kpis <-
     jsonlite::fromJSON(httr::content(response, "text", encoding = "UTF-8"))$data
-  
+
   # unnest scorings and full join with kpi list to ensure all kpis are present
   events <- events %>%
     tidyr::unnest(scorings, keep_empty = TRUE) %>%
@@ -154,7 +154,7 @@ getEventData <- function (match, token) {
       values_fill = 0
     ) %>%
     dplyr::filter(base::is.na(eventNumber) == FALSE)
-  
+
   # rename some columns
   events <- events %>%
     dplyr::rename(
@@ -164,7 +164,7 @@ getEventData <- function (match, token) {
       playerDetailedPosition = playerPositionDetailedPosition,
       duelType = duelDuelType
     )
-  
+
   # reorder columns
   # define desired column order
   attribute_cols <- base::c(
@@ -243,19 +243,19 @@ getEventData <- function (match, token) {
     "fouledPlayerId",
     "fouledPlayerName"
   )
-  
+
   # get list of kpi columns
   kpi_cols <- kpis$kpiName
-  
+
   # create order
   order <- base::c(attribute_cols, kpi_cols)
-  
+
   # reorder data
   events <- events[, order]
-  
+
   # reorder rows
   events <- events %>%
     dplyr::arrange(matchId, eventNumber)
-  
+
   return(events)
 }
