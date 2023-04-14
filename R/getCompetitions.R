6028e1e7de0c3ea26777d91f2315e5c79d42914d
Florian Schmitt <florian.schmitt94@gmail.com>
2023-04-14 14:44:19 +0200

    remove title & require, setup roxygen2

--- R/getCompetitions.R
+++ R/getCompetitions.R
@@ -1,33 +1,37 @@
-######
-#
-# This function returns a dataframe containing all competitionIterations available to the user
-#
-######
-
+#' Return a dataframe containing all competitionIterations available to the user
+#'
+#' @param token bearer token
+#'
+#' @return a dataframe containing all competition iterations available to the user
+#' @export
+#' @importFrom dplyr %>%
+#'
+#' @examples
+#' \donttest{
+#' try({ # prevent cran errors
+#'   competitionIterations <- getCompetitions(token)
+#' })
+#' }
 getCompetitions <- function(token) {
-  # require packages
-  require(httr)
-  require(jsonlite)
-  
   # request competition iteration information from API
   response <-
     httr::GET(url = "https://api.impect.com/v4/customerapi/scouting/competitionIterations/",
               httr::add_headers(Authorization = base::paste("Bearer", token, sep = " ")))
-  
+
   # check response status
   httr::stop_for_status(response)
-  
+
   # get data from response
   data <-
     jsonlite::fromJSON(httr::content(response, "text", encoding = "UTF-8"))$data
-  
+
   # convert to dataframe
   df <- jsonlite::flatten(base::as.data.frame(data))
-  
+
   # fix column names using regex
   base::names(df) <-
     base::gsub("\\.(.)", "\\U\\1", base::names(df), perl = TRUE)
-  
+
   # return dataframe
   return(df)
 }
