95c82a56dcf521327a26ac138216206b2b30edce
Florian Schmitt <florian.schmitt94@gmail.com>
2023-06-30 16:27:00 +0200

    get data from API version V5

--- R/getMatchplan.R
+++ R/getMatchplan.R
@@ -1,11 +1,13 @@
-#' Return a dataframe with basic information for all matches for a given competitionIteration
+#' Return a dataframe with basic information for all matches for a given iteration ID
 #'
-#' @param competitionIterationId Impect competition iteration ID
+#' @param iteration IMPECT iteration ID
 #' @param token bearer token
 #'
-#' @return a dataframe containing all matches for a given competition iteration ID
 #' @export
+#'
 #' @importFrom dplyr %>%
+#' @return a dataframe containing all matches for a given iteration ID
+#'
 #'
 #' @examples
 #' \donttest{
@@ -13,54 +15,79 @@
 #'   matchplan <- getMatchplan(518, token)
 #' })
 #' }
-getMatchplan <- function(competitionIterationId, token) {
-  # get competition iteration data
-  response <-
-    httr::RETRY(
-      "GET",
-      url = base::paste0(
-        "https://api.impect.com/v4/customerapi/scouting/competitionIterations/",
-        competitionIterationId
-      ),
-      httr::add_headers(
-        Authorization = base::paste("Bearer", token, sep =" "))
+getMatchplan <- function(iteration, token) {
+  # get matches
+  matches <- .matches(iteration = iteration, token = token)
+
+  # clean data
+  matches <- .cleanData(matches)
+
+  # get squads
+  squads <- .squadNames(iteration = iteration, token = token)
+
+  # clean data
+  squads <- .cleanData(squads)
+
+  # merge matchplan with squads
+  matches <- matches %>%
+    dplyr::left_join(squads,
+                     by = c("homeSquadId" = "id"),
+                     suffix = c("", "_home")) %>%
+    dplyr::rename(
+      homeSquadName = name,
+      homeType = type,
+      homeSquadSkillCornerId = skillCornerId_home,
+      homeSquadHeimSpielId = heimSpielId_home,
+      homeSquadCountryId = countryId
+    ) %>%
+    dplyr::left_join(squads,
+                     by = c("awaySquadId" = "id"),
+                     suffix = c("", "_away")) %>%
+    dplyr::rename(
+      awaySquadName = name,
+      awayType = type,
+      awaySquadSkillCornerId = skillCornerId_away,
+      awaySquadHeimSpielId = heimSpielId_away,
+      awaySquadCountryId = countryId
     )
 
-  # break if status code != 200
-  if (httr::http_error(response)) {
-    stop(
-      base::paste0(
-        "HTTP ",
-        httr::content(response)$status,
-        ": ",
-        httr::content(response)$message
-      )
+  # get countries data
+  countries <- .countryNames(token)
+
+  # merge matchplan with countries
+  matches <- matches %>%
+    dplyr::left_join(countries, by = c("homeSquadCountryId" = "id")) %>%
+    dplyr::rename(homeSquadCountryName = name) %>%
+    dplyr::left_join(countries, by = c("awaySquadCountryId" = "id")) %>%
+    dplyr::rename(awaySquadCountryName = name)
+
+  # reorder columns
+  matches <- matches %>%
+    dplyr::select(
+      iterationId,
+      matchId = id,
+      skillCornerId,
+      heimSpielId,
+      matchDayIndex,
+      matchDayName,
+      homeSquadId,
+      homeSquadName,
+      homeType,
+      homeSquadCountryId,
+      homeSquadCountryName,
+      homeSquadSkillCornerId,
+      homeSquadHeimSpielId,
+      awaySquadId,
+      awaySquadName,
+      awayType,
+      awaySquadCountryId,
+      awaySquadCountryName,
+      awaySquadSkillCornerId,
+      awaySquadHeimSpielId,
+      scheduledDate,
+      lastCalculationDate,
+      available
     )
-  }
-
-  # get data from response
-  data <-
-    jsonlite::fromJSON(httr::content(response, "text", encoding = "UTF-8"))$data
-
-  # create empty df
-  matches <- base::data.frame()
-
-  # iterate over competitionIterationSteps
-  for (i in 1:base::length(data$competitionIterationSteps$matches)) {
-    # get matches for iteration
-    temp <- data$competitionIterationSteps$matches[[i]]
-    # append to matches dataframe
-    matches <- dplyr::bind_rows(matches, temp)
-  }
-
-  # convert to dataframe
-  matches <- jsonlite::flatten(matches)
-
-  # fix column names using regex
-  base::names(matches) <-
-    gsub("\\.(.)", "\\U\\1", base::names(matches), perl = TRUE)
-  base::names(matches) <-
-    gsub("Competition", "", base::names(matches), perl = TRUE)
 
   # return matches
   return(matches)
