0bb993e76784542ec656053deca2a26bd938c7ba
Florian Schmitt <50988253+Flosch1006@users.noreply.github.com>
2024-10-16 11:11:08 +0200

    add function to retrieve player profile scores inlcuding documentation

---
+++ R/getPlayerProfileScores.R
@@ -0,0 +1,122 @@
+#' Return a dataframe that contains all player profile scores for a given
+#' iteration ID
+#'
+#' @param iteration Impect iteration ID
+#' @param positions list of position names
+#' @param token bearer token
+#'
+#' @export
+#'
+#' @importFrom dplyr %>%
+#' @return a dataframe containing the player profilescores aggregated per player
+#' for the given iteration ID and list of positions
+#'
+#' @examples
+#' \donttest{
+#' try({ # prevent cran errors
+#'   scores <- getPlayerProfileScores(518, token)
+#' })
+#' }
+getPlayerProfileScores <- function (iteration, positions, token) {
+  # check if iteration input is a string or integer
+  if (!(base::is.numeric(iteration) ||
+        base::is.character(iteration))) {
+    stop("Unprocessable type for 'iteration' variable")
+  }
+
+  # compile position string
+  position_string <- paste(positions, collapse = ",")
+
+  # get squads for given iterationId
+  squads <- .squadNames(iteration = iteration, token = token)
+  squadIds <- squads %>%
+    dplyr::filter(access == TRUE) %>%
+    dplyr::pull(id) %>%
+    base::unique()
+
+  # apply .playerProfileScores function to all squads
+  scores_raw <-
+    purrr::map_df(squadIds,
+                  ~ .playerProfileScores(
+                    iteration = iteration,
+                    squad = .,
+                    positions = position_string,
+                    token = token
+                  ))
+
+  # apply .playerNames function to a set of iterations
+  players <- .playerNames(iteration = iteration, token = token)
+
+  # get profile names
+  profile_list <- .playerProfiles(token = token)
+
+  # get competitions
+  iterations <- getIterations(token = token)
+
+  # manipulate averages
+
+  # unnest profile scores
+  scores <- scores_raw %>%
+    tidyr::unnest("profileScores", keep_empty = TRUE) %>%
+    dplyr::select(
+      iterationId,
+      squadId,
+      playerId,
+      playDuration,
+      matchShare,
+      profileName,
+      value
+    ) %>%
+    # add column to store positions string
+    dplyr::mutate(positions = position_string) %>%
+    # join with profiles to ensure all profiles are present and order by profile
+    dplyr::full_join(profile_list, by = c("profileName" = "name")) %>%
+    dplyr::arrange(profileName, playerId) %>%
+    # pivot data
+    tidyr::pivot_wider(
+      names_from = profileName,
+      values_from = value,
+      values_fill = 0,
+      values_fn = base::sum
+    ) %>%
+    # filter for non NA columns that were created by full join
+    dplyr::filter(base::is.na(playerId) == FALSE) %>%
+    # remove the "NA" column if it exists
+    dplyr::select(-dplyr::matches("^NA$"))
+
+  # merge with other data
+  scores <- scores %>%
+    dplyr::left_join(dplyr::select(squads, id, squadName = name),
+                     by = c("squadId" = "id")) %>%
+    dplyr::left_join(dplyr::select(players, id, playerName = commonname, firstname, lastname, birthdate, birthplace, leg),
+                     by = c("playerId" = "id")) %>%
+    dplyr::left_join(dplyr::select(iterations, id, competitionName, season),
+                     by = c("iterationId" = "id"))
+
+  # define column order
+  order <- c(
+    "iterationId",
+    "competitionName",
+    "season",
+    "squadId",
+    "squadName",
+    "playerId",
+    "playerName",
+    "firstname",
+    "lastname",
+    "birthdate",
+    "birthplace",
+    "leg",
+    "positions",
+    "matchShare",
+    "playDuration",
+    profile_list$name
+  )
+
+  # select columns
+  scores <- scores %>%
+    dplyr::select(dplyr::all_of(order))
+
+  # return scores
+  return(scores)
+}
