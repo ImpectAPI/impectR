92e1b3ea6e5045b2a2dc51bc358828e1a96659d6
Florian Schmitt <50988253+Flosch1006@users.noreply.github.com>
2024-10-16 11:15:18 +0200

    26 player squad scores ratios (#28)
    * update namespace
    
    * update namespace
    
    * add function to retrieve player iteration scores inlcuding documentation
    
    * add function to retrieve player match scores inlcuding documentation
    
    * add function to retrieve player profile scores inlcuding documentation
    
    * add function to retrieve squad iteration scores inlcuding documentation
    
    * add function to retrieve squad match scores inlcuding documentation
    
    * add several new helper functions for the new score functions

---
+++ R/getPlayerMatchScores.R
@@ -0,0 +1,218 @@
+#' Return a dataframe that contains all player scores for a given match ID
+#' and list of positions
+#'
+#' @param matches Impect match IDs
+#' @param positions list of position names
+#' @param token bearer token
+#'
+#' @export
+#'
+#' @importFrom dplyr %>%
+#' @return a dataframe containing the scores aggregated per player and
+#' position for the given match ID and list of positions
+#'
+#' @examples
+#' \donttest{
+#' try({ # prevent cran errors
+#'   playerMatchScores <- getPlayerMatchsums(84248, token)
+#' })
+#' }
+getPlayerMatchScores <- function (matches, positions, token) {
+  # check if match input is a list and convert to list if required
+  if (!base::is.list(matches)) {
+    if (base::is.numeric(matches) || base::is.character(matches)) {
+      matches <- base::c(matches)
+    } else {
+      stop("Unprocessable type for 'matches' variable")
+    }
+  }
+
+  # apply .matchInfo function to a set of matches
+  matchInfo <-
+    purrr::map_df(matches, ~ .matchInfo(match = ., token = token)) %>%
+    dplyr::select(id, iterationId, lastCalculationDate) %>%
+    base::unique()
+
+  # filter for fail matches
+  fail_matches <- matchInfo %>%
+    dplyr::filter(base::is.na(lastCalculationDate) == TRUE) %>%
+    dplyr::pull(id)
+
+  # filter for avilable matches
+  matches <- matchInfo %>%
+    dplyr::filter(base::is.na(lastCalculationDate) == FALSE) %>%
+    dplyr::pull(id)
+
+  # raise warnings
+  if (base::length(fail_matches) > 0) {
+    if (base::length(matches) == 0) {
+      base::stop("All supplied matches are unavailable. Execution stopped.")
+    }
+    else {
+      base::warning(
+        sprintf(
+          "The following matches are not available yet and were ignored:\n\t%s",
+          paste(fail_matches, collapse = ", ")
+        )
+      )
+    }
+  }
+
+  # compile position string
+  position_string <- paste(positions, collapse = ",")
+
+  # apply .playerMatchScores function to a set of matches
+  scores_raw <-
+    purrr::map(matches, ~ .playerMatchScores(
+      match = .,
+      positions = position_string,
+      token = token
+      ))
+
+  # get unique iterationIds
+  iterations <- matchInfo %>%
+    dplyr::pull(iterationId) %>%
+    base::unique()
+
+  # apply playerNames function to a set of iterations
+  players <-
+    purrr::map_df(iterations, ~ .playerNames(iteration = ., token = token)) %>%
+    dplyr::select(id, playerName = commonname, firstname, lastname, birthdate, birthplace, leg) %>%
+    base::unique()
+
+  # apply squadNames function to a set of iterations
+  squads <-
+    purrr::map_df(iterations, ~ .squadNames(iteration = ., token = token)) %>%
+    dplyr::select(id, name) %>%
+    base::unique()
+
+  # get kpi names
+  score_list <- .playerScores(token = token)
+
+  # get matchplan data
+  matchplan <-
+    purrr::map_df(iterations, ~ getMatches(iteration = ., token = token))
+
+  # get competitions
+  iterations <- getIterations(token = token)
+
+  # manipulate scores
+
+  extract_scores <- function(dict, side) {
+    # convert side data to df
+    temp <-
+      jsonlite::flatten(jsonlite::fromJSON(
+        jsonlite::toJSON(dict[side][[1]]$players),
+        simplifyDataFrame = TRUE
+      ),
+      recursive = FALSE)
+
+    # unnest scores
+    temp <- temp %>%
+      tidyr::unnest("playerScores", keep_empty = TRUE) %>%
+      dplyr::select(
+        "playerId" = "id",
+        "playerScoreId",
+        "value",
+        "matchShare",
+        "playDuration") %>%
+      # join with kpis to ensure all kpiIds are present and order by kpiId
+      dplyr::full_join(score_list, by = c("playerScoreId" = "id")) %>%
+      dplyr::arrange("playerScoreId", "playerId") %>%
+      # drop kpiId column
+      dplyr::select(-"playerScoreId") %>%
+      # pivot data
+      tidyr::pivot_wider(
+        names_from = "name",
+        values_from = "value",
+        values_fill = 0,
+        values_fn = base::sum
+      ) %>%
+      # filter for non NA columns that were created by full join
+      dplyr::filter(base::is.na(playerId) == FALSE) %>%
+      # remove the "NA" column if it exists
+      dplyr::select(-dplyr::matches("^NA$")) %>%
+      dplyr::mutate(
+        # add matchId
+        matchId = dict$matchId,
+        # add squadId
+        squadId = dict[[side]]$id,
+        # add positions
+        positions = position_string)
+
+    return(temp)
+  }
+
+  #create emtpty df to store match scores
+  scores <- data.frame()
+
+  # iterate over list elements
+  for (i in 1:length(scores_raw)) {
+    temp <- scores_raw[[i]]
+    # apply extract_matchsums function to home and away squad
+    temp <-
+      purrr::map_df(
+        base::c("squadHome", "squadAway"),
+        ~ extract_scores(temp, .)
+      )
+
+    # appénd to target df
+    scores <- base::rbind(scores, temp)
+  }
+
+  # merge with other data
+  scores <- scores %>%
+    dplyr::left_join(
+      matchplan,
+      by = c("matchId" = "id")
+    ) %>%
+    dplyr::left_join(
+      iterations,
+      by = c("iterationId" = "id")
+    ) %>%
+    dplyr::left_join(
+      dplyr::select(squads, id, squadName = name),
+      by = c("squadId" = "id")
+    ) %>%
+    dplyr::left_join(
+      dplyr::select(players, id, playerName, firstname, lastname, birthdate, birthplace, leg),
+      by = c("playerId" = "id")
+    ) %>%
+    # fix some column names
+    dplyr::rename(
+      dateTime = scheduledDate
+    )
+
+  # define column order
+  order <- c(
+    "matchId",
+    "dateTime",
+    "competitionName",
+    "competitionId",
+    "competitionType",
+    "iterationId",
+    "season",
+    "matchDayIndex",
+    "matchDayName",
+    "squadId",
+    "squadName",
+    "playerId",
+    "playerName",
+    "firstname",
+    "lastname",
+    "birthdate",
+    "birthplace",
+    "leg",
+    "positions",
+    "matchShare",
+    "playDuration",
+    score_list$name
+  )
+
+  # select columns
+  scores <- scores %>%
+    dplyr::select(dplyr::all_of(order))
+
+  # return matchsums
+  return(scores)
+}
---
+++ R/getSquadMatchScores.R
@@ -0,0 +1,173 @@
+#' Return a dataframe that contains squad level scores and ratios for a given match ID
+#'
+#' @param matches Impect match IDs
+#' @param token bearer token
+#'
+#' @export
+#'
+#' @importFrom dplyr %>%
+#' @return a dataframe containing the scores and rations aggregated per squad for the given match ID
+#'
+#' @examples
+#' \donttest{
+#' try({ # prevent cran errors
+#'   matchsums <- getSquadMatchsums(84248, token)
+#' })
+#' }
+getSquadMatchScores <- function (matches, token) {
+  # check if match input is a list and convert to list if required
+  if (!base::is.list(matches)) {
+    if (base::is.numeric(matches) || base::is.character(matches)) {
+      matches <- base::c(matches)
+    } else {
+      stop("Unprocessable type for 'matches' variable")
+    }
+  }
+
+  # apply .matchInfo function to a set of matches
+  matchInfo <-
+    purrr::map_df(matches, ~ .matchInfo(match = ., token = token)) %>%
+    dplyr::select(id, iterationId, lastCalculationDate) %>%
+    base::unique()
+
+  # filter for fail matches
+  fail_matches <- matchInfo %>%
+    dplyr::filter(base::is.na(lastCalculationDate) == TRUE) %>%
+    dplyr::pull(id)
+
+  # filter for avilable matches
+  matches <- matchInfo %>%
+    dplyr::filter(base::is.na(lastCalculationDate) == FALSE) %>%
+    dplyr::pull(id)
+
+  # raise warnings
+  if (base::length(fail_matches) > 0) {
+    if (base::length(matches) == 0) {
+      base::stop("All supplied matches are unavailable. Execution stopped.")
+    }
+    else {
+      base::warning(
+        sprintf(
+          "The following matches are not available yet and were ignored:\n\t%s",
+          paste(fail_matches, collapse = ", ")
+        )
+      )
+    }
+  }
+
+  # apply _eventAttributes function to a set of matches
+  scores_raw <-
+    purrr::map(matches, ~ .squadMatchScores(match = ., token = token))
+
+  # get unique iterationIds
+  iterations <- matchInfo %>%
+    dplyr::pull(iterationId) %>%
+    base::unique()
+
+  # apply squadNames function to a set of iterations
+  squads <-
+    purrr::map_df(iterations, ~ .squadNames(iteration = ., token = token)) %>%
+    dplyr::select(id, name) %>%
+    base::unique()
+
+  # get score names
+  scores_list <- .squadScores(token = token)
+
+  # get matchplan data
+  matchplan <-
+    purrr::map_df(iterations, ~ getMatches(iteration = ., token = token))
+
+  # get competitions
+  iterations <- getIterations(token = token)
+
+  # manipulate matchsums
+
+  # define function to extract match scores
+  extract_scores <- function(dict, side) {
+    # convert side data to df
+    temp <-
+      base::data.frame(dict[[side]]) %>%
+      dplyr::rename(squadScoreId = squadScores.squadScoreId,
+                    value = squadScores.value)
+
+    # unnest scores
+    temp <- temp %>%
+      # join with scores to ensure all squadScoreId are present and order by squadScoreId
+      dplyr::full_join(scores_list, by = c("squadScoreId" = "id")) %>%
+      dplyr::arrange(squadScoreId, id) %>%
+      # drop squadScoreId column
+      dplyr::select(-squadScoreId) %>%
+      # pivot data
+      tidyr::pivot_wider(
+        names_from = name,
+        values_from = value,
+        values_fill = 0,
+        values_fn = base::sum
+      ) %>%
+      # filter for non NA columns that were created by full join
+      dplyr::filter(base::is.na(id) == FALSE) %>%
+      dplyr::mutate(
+        # add matchId
+        matchId = dict$matchId)
+
+    return(temp)
+  }
+
+  #create emtpty df to store matchsums
+  scores <- data.frame()
+
+  # iterate over list elements
+  for (i in 1:length(scores_raw)) {
+    temp <- scores_raw[[i]]
+    # apply extract_matchsums function to home and away squad
+    temp <-
+      purrr::map_df(base::c("squadHome", "squadAway"),
+                    ~ extract_scores(temp, .))
+
+    # appénd to target df
+    scores <- base::rbind(scores, temp)
+  }
+
+  # merge with other data
+  scores <- scores %>%
+    dplyr::left_join(
+      matchplan,
+      by = c("matchId" = "id")
+    ) %>%
+    dplyr::left_join(
+      iterations,
+      by = c("iterationId" = "id")
+    ) %>%
+    dplyr::left_join(
+      dplyr::select(squads, id, squadName = name),
+      by = c("id" = "id")
+    ) %>%
+    # fix some column names
+    dplyr::rename(
+      dateTime = scheduledDate,
+      squadId = id
+    )
+
+  # define column order
+  order <- c(
+    "matchId",
+    "dateTime",
+    "competitionName",
+    "competitionId",
+    "competitionType",
+    "iterationId",
+    "season",
+    "matchDayIndex",
+    "matchDayName",
+    "squadId",
+    "squadName",
+    scores_list$name
+  )
+
+  # select columns
+  scores <- scores %>%
+    dplyr::select(dplyr::all_of(order))
+
+  # return matchsums
+  return(scores)
+}
