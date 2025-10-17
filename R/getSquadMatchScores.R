802339a34d6ae7d1770e3b43c42bd45e284adb47
Florian Schmitt <florian.schmitt94@gmail.com>
2025-10-17 13:17:52 +0200

    handle the edge case where coaches endpoint is blacklisted for user

--- R/getSquadMatchScores.R
+++ R/getSquadMatchScores.R
@@ -147,33 +147,48 @@ getSquadMatchScores <- function (
   squads <- .cleanData(squads)
 
   # get coach master data from API
+  coaches_blacklisted = FALSE
   coaches <-
     purrr::map_df(
       iterations,
       ~ {
-        response <- jsonlite::fromJSON(
-          httr::content(
-            .callAPIlimited(
-              host,
-              base_url = "/v5/customerapi/iterations/",
-              id = .,
-              suffix = "/coaches",
-              token = token
-            ),
-            "text",
-            encoding = "UTF-8"
-          )
-        )$data
+        response <- .callAPIlimited(
+          host,
+          base_url = "/v5/customerapi/iterations/",
+          id = .,
+          suffix = "/coaches",
+          token = token,
+          ignore_403 = TRUE
+        )
 
-        if (base::length(response) > 0) {
-          response <- response %>%
-            jsonlite::flatten()
-        } else {
+        # check status
+        status <- httr::status_code(response)
+
+        if (status == 403) {
+          coaches_blacklisted <<- TRUE
+
+          # insert empty df as response
           response <- base::data.frame(
             id = -1,
             name = "",
             stringsAsFactors = FALSE
           )
+        } else {
+          response <- jsonlite::fromJSON(
+            httr::content(response, "text", encoding = "UTF-8")
+          )$data
+
+          # flatten response
+          if (base::length(response) > 0) {
+            response <- response %>%
+              jsonlite::flatten()
+          } else {
+            response <- base::data.frame(
+              id = -1,
+              name = "",
+              stringsAsFactors = FALSE
+            )
+          }
         }
       }
     ) %>%
@@ -297,18 +312,23 @@ getSquadMatchScores <- function (
       ),
       by = base::c("matchId" = "matchId", "squadId" = "squadId")
     ) %>%
-    dplyr::left_join(
-      dplyr::select(
-        coaches,
-        coachId = .data$id,
-        coachName = .data$name
-      ),
-      by = base::c("coachId" = "coachId")
-    ) %>%
     # fix some column names
     dplyr::rename(
       dateTime = .data$scheduledDate
     )
+
+  # merge events with coaches
+  if (coaches_blacklisted == FALSE) {
+    scores <- scores %>%
+      dplyr::left_join(
+        dplyr::select(
+          coaches,
+          coachId = .data$id,
+          coachName = .data$name
+        ),
+        by = base::c("coachId" = "coachId")
+      )
+  }
 
   # define column order
   order <- c(
@@ -330,6 +350,11 @@ getSquadMatchScores <- function (
     "coachName",
     scores_list$name
   )
+
+  # check if coaches are blacklisted
+  if (coaches_blacklisted) {
+    order <- order[!order %in% c("coachId", "coachName")]
+  }
 
   # select columns
   scores <- scores %>%
